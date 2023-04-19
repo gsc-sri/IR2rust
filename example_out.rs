c546a30023017e9a94d422faabcdc73913d596e2
Nathan Gasc <nathan.gasc@sri.com>
2023-04-18 17:11:56 -0700

    Updated example

--- example_out.rs
+++ example_out.rs
@@ -1,78 +1,64 @@
 
 /// IR SRC :
-/// 
-///(lambda ((ivar_1 (subrange 0 * nil nil) n))
+////// 
+///fibo__f
+///(lambda ((ivar_1 (subrange 1 * nil nil) n))
 ///  '->
-///  (subrange 0 * nil nil)
-///  (let ivar_2
-///    boolean
+///  (-> (subrange 0 * nil nil) (subrange 0 * nil nil))
+///  (lambda ((ivar_3 (subrange 0 * nil nil) a))
+///    '->
+///    (subrange 0 * nil nil)
 ///    (let ivar_4
-///      (subrange 2 2 nil nil)
-///      2
-///      (< (ivar_1 (subrange 0 * nil nil) n)
-///         (last (ivar_4 (subrange 2 2 nil nil) nil)) nil))
-///    (if (ivar_2 boolean nil)
-///        (last (ivar_1 (subrange 0 * nil nil) n))
-///      (let ivar_7
-///        (subrange 0 * nil nil)
-///        (let ivar_15
+///      boolean
+///      (< (ivar_3 (subrange 0 * nil nil) a)
+///         (ivar_1 (subrange 1 * nil nil) n) nil)
+///      (if (ivar_4 boolean nil)
+///          (release ((ivar_3 (subrange 0 * nil nil) a)) nil 0)
+///        (let ivar_11
 ///          (subrange 0 * nil nil)
-///          (let ivar_11
-///            (subrange 1 1 nil nil)
-///            1
-///            (- (ivar_1 (subrange 0 * nil nil) n)
-///               (last (ivar_11 (subrange 1 1 nil nil) nil))
-///               (rename
-///                (((ivar_10 (subrange 0 * nil nil) nil) ivar_1
-///                  (subrange 0 * nil nil) n))
-///                in (subrange -1 * nil nil))))
-///          (is_prime__fibo (last (ivar_15 (subrange 0 * nil nil) nil))
-///           nil))
-///        (let ivar_8
-///          (subrange 0 * nil nil)
-///          (let ivar_22
-///            (subrange 0 * nil nil)
-///            (let ivar_18
-///              (subrange 2 2 nil nil)
-///              2
-///              (- (last (ivar_1 (subrange 0 * nil nil) n))
-///                 (last (ivar_18 (subrange 2 2 nil nil) nil))
+///          (let ivar_20
+///            (-> (subrange 0 * nil nil) (subrange 0 * nil nil))
+///            (fibo__f (ivar_1 (subrange 1 * nil nil) n) nil)
+///            (let ivar_26
+///              (subrange 0 * nil nil)
+///              (- (last (ivar_3 (subrange 0 * nil nil) a))
+///                 (ivar_1 (subrange 1 * nil nil) n)
 ///                 (rename
-///                  (((ivar_17 (subrange 0 * nil nil) nil) ivar_1
-///                    (subrange 0 * nil nil) n))
-///                  in (subrange -2 * nil nil))))
-///            (is_prime__fibo (last (ivar_22 (subrange 0 * nil nil) nil))
-///             nil))
-///          (+ (last (ivar_7 (subrange 0 * nil nil) nil))
-///             (last (ivar_8 (subrange 0 * nil nil) nil))
-///             (subrange 0 * nil nil))))))
+///                  (((ivar_14 (subrange 1 * nil nil) nil) ivar_1
+///                    (subrange 1 * nil nil) n)
+///                   ((ivar_13 (subrange 0 * nil nil) nil) ivar_3
+///                    (subrange 0 * nil nil) a))
+///                  in (subrange * * nil nil)))
+///              ((last (ivar_20
+///                      (-> (subrange 0 * nil nil)
+///                       (subrange 0 * nil nil))
+///                      nil))
+///               (last (ivar_26 (subrange 0 * nil nil) nil)) nil)))
+///          (+ 1 (last (ivar_11 (subrange 0 * nil nil) nil))
+///             (subrange 1 * nil nil)))))
+///    ((ivar_1 (subrange 1 * nil nil) n)))
 ///  nil)
-
 
 /// OUTPUT:
 use rug::Integer;
-fn is_prime__fibo(ivar_1: Integer) -> Integer {
-    let ivar_2: bool = {
-        let ivar_4: Integer = 2;
-        ivar_1 < ivar_4
-    };
-    if ivar_2 {
-        ivar_1
-    } else {
-        let ivar_7: Integer = {
-            let ivar_15: Integer = {
-                let ivar_11: Integer = 1;
-                ivar_1 - ivar_11
+
+fn fibo_f(ivar_1: Integer) -> Box<dyn Fn(Integer) -> Integer> {
+    Box::new(move |ivar_3: Integer| -> Integer {
+        let ivar_4: bool = { ivar_3 < ivar_1.clone() };
+        if ivar_4 {
+            Integer::from(0)
+        } else {
+            let ivar_11: Integer = {
+                let ivar_20: Box<dyn Fn(Integer) -> Integer> = { fibo_f(ivar_1.clone()) };
+                let ivar_26: Integer = { ivar_3 - ivar_1.clone() };
+                ivar_20(ivar_26)
             };
-            is_prime__fibo(ivar_15)
-        };
-        let ivar_8: Integer = {
-            let ivar_22: Integer = {
-                let ivar_18: Integer = 2;
-                ivar_1 - ivar_18
-            };
-            is_prime__fibo(ivar_22)
-        };
-        ivar_7 + ivar_8
-    }
+            Integer::from(1) + ivar_11
+        }
+    })
+}
+
+/// TEST:
+fn main(){
+    println!("{}", fibo_f(Integer::from(5))(Integer::from(17)))
 }
