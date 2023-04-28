bd9f5e11e68a55634e9e54cc3df605493d5323b8
Nathan Gasc <nathan.gasc@sri.com>
2023-04-28 10:41:49 -0700

    updated example

--- example_out.rs
+++ example_out.rs
@@ -1,91 +1,146 @@
 
 /// IR SRC :
 
-///smalltranspose__test_transpose
-///(let ivar_1
-///  (-> (subrange 0 99 nil nil)
-///   (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)))
-///  (lambda ((ivar_5 (subrange 0 99 nil nil) nil))
-///    '->
-///    (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))
-///    (smalltranspose__mkRow (last (ivar_5 (subrange 0 99 nil nil) nil))
-///     (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)))
-///    nil)
-///  (let ivar_18
-///    (-> (subrange 0 99 nil nil)
-///     (-> (subrange 0 99 nil nil)
-///      (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))))
-///    (lambda ((ivar_8 (subrange 0 99 nil nil) i))
-///      '->
-///      (-> (subrange 0 99 nil nil)
-///       (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)))
-///      (ivar_1
-///       (-> (subrange 0 99 nil nil)
-///        (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)))
-///       Z)
-///      ((ivar_1
-///        (-> (subrange 0 99 nil nil)
-///         (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)))
-///        Z)))
-///    (lett ivar_15
-///     (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))[99/99]))[99/99]))
-///     (-> (subrange 0 99 nil nil)
-///      (-> (subrange 0 99 nil nil)
-///       (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))))
-///     (last (ivar_18
-///            (-> (subrange 0 99 nil nil)
-///             (-> (subrange 0 99 nil nil)
-///              (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))))
-///            nil))
-///     (let ivar_16
-///       (subrange 0 100 nil nil)
-///       0
-///       (smalltranspose__transpose
-///        (last (ivar_15
-///               (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))[99/99]))[99/99]))
-///               nil))
-///        (last (ivar_16 (subrange 0 100 nil nil) nil)) nil))))
+///smalltranspose__transpose_step
+///(lambda ((ivar_1
+///          (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///          X)
+///         (ivar_2 (subrange 0 99 nil nil) i)
+///         (ivar_3 (subrange 1 100 nil nil) j))
+///  '->
+///  (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///  (let ivar_4
+///    boolean
+///    (= (ivar_3 (subrange 1 100 nil nil) j) 100 nil)
+///    (if (ivar_4 boolean nil)
+///        (release
+///         ((ivar_3 (subrange 1 100 nil nil) j)
+///          (ivar_2 (subrange 0 99 nil nil) i))
+///         nil
+///         (last (ivar_1
+///                (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                X)))
+///      (let ivar_9
+///        (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///        (let ivar_11
+///          (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))
+///          (let ivar_16
+///            (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99]))
+///            (lookup (ivar_1
+///                     (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                     X)
+///                    (ivar_3 (subrange 1 100 nil nil) j))
+///            (lookup (last (ivar_16
+///                           (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99]))
+///                           nil))
+///                    (ivar_2 (subrange 0 99 nil nil) i)))
+///          (let ivar_12
+///            (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))
+///            (let ivar_27
+///              (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99]))
+///              (lookup (ivar_1
+///                       (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                       X)
+///                      (ivar_2 (subrange 0 99 nil nil) i))
+///              (lookup (last (ivar_27
+///                             (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99]))
+///                             nil))
+///                      (ivar_3 (subrange 1 100 nil nil) j)))
+///            (let ivar_40
+///              (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///              (update
+///               (lookup (last (ivar_1
+///                              (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                              X))
+///                       ((ivar_2 (subrange 0 99 nil nil) i)))
+///               (ivar_3 (subrange 1 100 nil nil) j)
+///               (last (ivar_11
+///                      (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))
+///                      nil)))
+///              (update
+///               (lookup (last (ivar_40
+///                              (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                              nil))
+///                       ((ivar_3 (subrange 1 100 nil nil) j)))
+///               (ivar_2 (subrange 0 99 nil nil) i)
+///               (last (ivar_12
+///                      (smalltranspose__nat32 : (subrange 0 4294967295 nil nil))
+///                      nil))))))
+///        (let ivar_58
+///          (subrange 1 100 nil nil)
+///          (+ (last (ivar_3 (subrange 1 100 nil nil) j)) 1
+///             (rename
+///              (((ivar_51 (subrange 1 100 nil nil) nil) ivar_3
+///                (subrange 1 100 nil nil) j)
+///               ((ivar_57 (subrange 0 99 nil nil) nil) ivar_2
+///                (subrange 0 99 nil nil) i)
+///               ((ivar_56
+///                 (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                 nil)
+///                ivar_9
+///                (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                Y))
+///              in (subrange 2 101 nil nil)))
+///          (smalltranspose__transpose_step
+///           (last (ivar_9
+///                  (smalltranspose__square : (array (smalltranspose__line : (array (smalltranspose__nat32 : (subrange 0 4294967295 nil nil)) [99/99])) [99/99]))
+///                  Y))
+///           (last (ivar_2 (subrange 0 99 nil nil) i))
+///           (last (ivar_58 (subrange 1 100 nil nil) nil)) nil)))))
+///  nil)
 
 /// OUTPUT:
+use std::rc::Rc;
+
 #[allow(non_snake_case, dead_code)]
 fn smalltranspose__transpose_step(
-    mut ivar_1: [[Integer; 99]; 99],
-    ivar_2: Integer,
-    ivar_3: Integer
-) -> [[Integer; 99]; 99] {
-    let ivar_4: bool = { ivar_3 == Integer::from(99) };
-    if ivar_4 {
-        ivar_1
-    } else {
-        let ivar_9: [[Integer; 99]; 99] = {
-            let ivar_11: Integer = {
-                let ivar_16: [Integer; 99] = { ivar_1[ivar_3.to_usize_wrapping()].clone() };
-                ivar_16[ivar_2.to_usize_wrapping()].clone()
-            };
-            let ivar_12: Integer = {
-                let ivar_27: [Integer; 99] = { ivar_1[ivar_2.to_usize_wrapping()].clone() };
-                ivar_27[ivar_3.to_usize_wrapping()].clone()
-            };
-            let mut ivar_47: [[Integer; 99]; 99] = {
-                let mut ivar_36: [Integer; 99] = { ivar_1[ivar_2.to_usize_wrapping()].clone() };
-                let mut ivar_44: [[Integer; 99]; 99] = { ivar_1 };
-                let ivar_45: [Integer; 99] = {
-                    ivar_36[ivar_3.to_usize_wrapping()] = ivar_11;
-                    ivar_36
+    mut ivar_1: Rc<[Rc<[i32; SIZE]>; SIZE]>,
+    ivar_2: i32,
+    ivar_3: i32
+) -> Rc<[Rc<[i32; SIZE]>; SIZE]> {
+    {
+        let ivar_4: bool = { ivar_3 == SIZE as i32 };
+        if ivar_4 {
+            ivar_1
+        } else {
+            {
+                let ivar_9: Rc<[Rc<[i32; SIZE]>; SIZE]> = {
+                    {
+                        let ivar_11: i32 = {
+                            {
+                                let ivar_16: Rc<[i32; SIZE]> = { ivar_1[ivar_3 as usize].clone() };
+                                ivar_16[ivar_2 as usize].clone()
+                            }
+                        };
+                        {
+                            let ivar_12: i32 = {
+                                {
+                                    let ivar_27: Rc<[i32; SIZE]> = {
+                                        ivar_1[ivar_2 as usize].clone()
+                                    };
+                                    ivar_27[ivar_3 as usize].clone()
+                                }
+                            };
+                            {
+                                let mut ivar_40: Rc<[Rc<[i32; SIZE]>; SIZE]> = {
+                                    (*Rc::make_mut(
+                                        &mut (*Rc::make_mut(&mut ivar_1))[ivar_2 as usize]
+                                    ))[ivar_3 as usize] = ivar_11;
+                                    ivar_1
+                                };
+                                (*Rc::make_mut(
+                                    &mut (*Rc::make_mut(&mut ivar_40))[ivar_3 as usize]
+                                ))[ivar_2 as usize] = ivar_12;
+                                ivar_40
+                            }
+                        }
+                    }
                 };
-                ivar_44[ivar_2.to_usize_wrapping()] = ivar_45;
-                ivar_44
-            };
-            let mut ivar_48: [Integer; 99] = { ivar_47[ivar_3.to_usize_wrapping()].clone() };
-            let mut ivar_56: [[Integer; 99]; 99] = { ivar_47 };
-            let ivar_57: [Integer; 99] = {
-                ivar_48[ivar_2.to_usize_wrapping()] = ivar_12;
-                ivar_48
-            };
-            ivar_56[ivar_3.to_usize_wrapping()] = ivar_57;
-            ivar_56
-        };
-        let ivar_72: Integer = { ivar_3 + Integer::from(1) };
-        smalltranspose__transpose_step(ivar_9, ivar_2, ivar_72)
+                {
+                    let ivar_58: i32 = { ivar_3 + 1 };
+                    smalltranspose__transpose_step(ivar_9, ivar_2, ivar_58)
+                }
+            }
+        }
     }
 }
